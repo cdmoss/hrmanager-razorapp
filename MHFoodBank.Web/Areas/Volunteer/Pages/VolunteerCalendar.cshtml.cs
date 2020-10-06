b193353fc3c5f402a7eddc7959d11de3894d2605
chase <cdmossing@gmail.com>
2020-10-06 11:18:41 -0600

    added schedule to volunteer side, now adding sign up function

--- MHFoodBank.Web/Areas/Volunteer/Pages/VolunteerCalendar.cshtml.cs
+++ MHFoodBank.Web/Areas/Volunteer/Pages/VolunteerCalendar.cshtml.cs
@@ -15,6 +15,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MHFoodBank.Common.Services;
+using Syncfusion.EJ2;
+using Syncfusion.EJ2.Base;
 
 namespace MHFoodBank.Web.Areas.Volunteer.Pages
 {
@@ -45,15 +47,43 @@ namespace MHFoodBank.Web.Areas.Volunteer.Pages
         public async Task OnGet(string statusMessage = null)
         {
             StatusMessage = statusMessage;
-            await GetVolunteer();
+            await PrepareModel();
         }
 
-        public async Task<JsonResult> OnPostGetMyShifts()
+        public async Task<JsonResult> OnPostGetShifts([FromBody] CRUDModel model)
         {
-            var user = await _userManager.GetUserAsync(User);
-            await _context.Entry(user).Reference(u => u.VolunteerProfile).LoadAsync();
-            await _context.Entry(user.VolunteerProfile).Collection(v => v.Shifts).LoadAsync();
-            var userShifts = user.VolunteerProfile.Shifts;
+            List<Shift> displayedShifts = new List<Shift>();
+
+            if (model.Params.ContainsKey("type"))
+            {
+                if (model.Params["type"].ToString() == "open")
+                {
+                    var openShifts = _context.Shifts.Where(s => s.VolunteerProfileId == null).ToList();
+                    displayedShifts = FilterShiftsByDate(openShifts);
+                }
+                else
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                    await _context.Entry(user).Reference(u => u.VolunteerProfile).LoadAsync();
+                    await _context.Entry(user.VolunteerProfile).Collection(v => v.Shifts).LoadAsync();
+                    var userShifts = user.VolunteerProfile.Shifts;
+                    displayedShifts = FilterShiftsByDate(userShifts);
+                }
+            }
+            else
+            {
+                var user = await _userManager.GetUserAsync(User);
+                await _context.Entry(user).Reference(u => u.VolunteerProfile).LoadAsync();
+                await _context.Entry(user.VolunteerProfile).Collection(v => v.Shifts).LoadAsync();
+                var userShifts = user.VolunteerProfile.Shifts;
+                displayedShifts = FilterShiftsByDate(userShifts);
+            }
+            
+            return new JsonResult(_mapper.Map<List<ShiftReadEditDto>>(displayedShifts));
+        }
+
+        private List<Shift> FilterShiftsByDate(IList<Shift> userShifts)
+        {
             bool shiftShouldBeDisplayed;
             CurrentDateFilter filter = new CurrentDateFilter();
             var displayedShifts = new List<Shift>();
@@ -73,41 +103,16 @@ namespace MHFoodBank.Web.Areas.Volunteer.Pages
                 }
             }
 
-            return new JsonResult(_mapper.Map<List<ShiftReadEditDto>>(displayedShifts));
+            return displayedShifts;
         }
 
-        public async Task<JsonResult> OnPostGetOpenShifts()
-        {
-            var openShifts = await _context.Shifts.Where(s => s.VolunteerProfileId == null).ToListAsync();
-            bool shiftShouldBeDisplayed;
-            CurrentDateFilter filter = new CurrentDateFilter();
-            var displayedShifts = new List<Shift>();
-            // this foreach iterates through all the shifts and determines whether or not they should be displayed
-            // and what color they should be displayed with (open vs assigned)
-            foreach (var s in openShifts)
-            {
-                // CheckIfShiftDateIsAfterToday will handle recurring shifts in a special way:
-                // it will check through all the shifts in it's recurrence set, if it finds one of the 
-                // shifts to be scheduled past todays date, it will exclude all the shifts from that set
-                // which are scheduled before todays date and display the rest
-                shiftShouldBeDisplayed = filter.CheckIfShiftDateIsAfterToday(s);
-
-                if (shiftShouldBeDisplayed)
-                {
-                    displayedShifts.Add(s);
-                }
-            }
-
-            return new JsonResult(_mapper.Map<List<ShiftReadEditDto>>(displayedShifts));
-        }
-
-        private async Task<VolunteerProfile> GetVolunteer()
+        private async Task<VolunteerProfile> PrepareModel()
         {
             var user = await _userManager.GetUserAsync(User);
             await _context.Entry(user).Reference(p => p.VolunteerProfile).LoadAsync();
-
+            Positions = await _context.Positions.Where(p => p.Name != "All").ToListAsync();
+            
             Approved = user.VolunteerProfile.ApprovalStatus;
-
 
             LoggedInUser = user.VolunteerProfile.FirstName + " " + user.VolunteerProfile.LastName;
 
